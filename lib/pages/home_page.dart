0122c3723ee7e17c5f123ddb020707744a89f9a7
Alex <Alex@DESKTOP-A4E41IH>
2022-01-21 20:57:19 +0200

    Beautify HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,58 +9,61 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  TextEditingController _searchController = new
-  TextEditingController();
+  TextEditingController _searchController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: getBody(),
     );
   }
-  Widget getBody(){
+
+  Widget getBody() {
     return SafeArea(
       child: ListView(
-        padding: const EdgeInsets.only(left: 20,right: 20,top: 15),
+        padding: const EdgeInsets.only(left: 20, right: 20, top: 15),
         children: <Widget>[
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-            Container(
-              width: 40,
-              height: 40,
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                  image: NetworkImage("https://media.istockphoto.com/photos/millennial-male-team-leader-organize-virtual-workshop-with-employees-picture-id1300972574?b=1&k=20&m=1300972574&s=170667a&w=0&h=2nBGC7tr0kWIU8zRQ3dMg-C5JLo9H2sNUuDjQ5mlYfo="),fit: BoxFit.cover)
+              Container(
+                width: 40,
+                height: 40,
+                decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: NetworkImage(
+                            "https://media.istockphoto.com/photos/millennial-male-team-leader-organize-virtual-workshop-with-employees-picture-id1300972574?b=1&k=20&m=1300972574&s=170667a&w=0&h=2nBGC7tr0kWIU8zRQ3dMg-C5JLo9H2sNUuDjQ5mlYfo="),
+                        fit: BoxFit.cover)),
               ),
-            ),
-            const Text("Chats",style:TextStyle(
-              fontSize: 22,
-              color: Colors.black,
-              fontWeight: FontWeight.bold
-            ),),
-            Icon(Icons.edit)
-          ],),
-          const SizedBox(height: 10,),
+              const Text(
+                "Chats",
+                style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold),
+              ),
+              Icon(Icons.edit)
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
           Container(
             width: double.infinity,
             height: 40,
             decoration: BoxDecoration(
-              color: Colors.grey,
-              borderRadius: BorderRadius.circular(10)
-            ),
+                color: Colors.grey, borderRadius: BorderRadius.circular(10)),
             child: Padding(
               padding: const EdgeInsets.only(left: 10),
-            child: TextField(
-              cursorColor: Colors.black,
-              controller: _searchController,
-              decoration: const InputDecoration(
-                hintText: "Search",
-                border: InputBorder.none,
-                prefixIcon: Icon(Icons.search,
-                color: Colors.black)
+              child: TextField(
+                cursorColor: Colors.black,
+                controller: _searchController,
+                decoration: const InputDecoration(
+                    hintText: "Search",
+                    border: InputBorder.none,
+                    prefixIcon: Icon(Icons.search, color: Colors.black)),
               ),
-            ),
             ),
           )
         ],
