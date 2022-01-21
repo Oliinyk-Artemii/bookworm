eaed4b54de7f69684da772daa5cfe3f65384d091
Artemii <a.oliinyk@star.global>
2022-01-21 20:45:50 +0200

    Initial commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController _searchController = new
+  TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: getBody(),
+    );
+  }
+  Widget getBody(){
+    return SafeArea(
+      child: ListView(
+        padding: const EdgeInsets.only(left: 20,right: 20,top: 15),
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+            Container(
+              width: 40,
+              height: 40,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: NetworkImage("https://media.istockphoto.com/photos/millennial-male-team-leader-organize-virtual-workshop-with-employees-picture-id1300972574?b=1&k=20&m=1300972574&s=170667a&w=0&h=2nBGC7tr0kWIU8zRQ3dMg-C5JLo9H2sNUuDjQ5mlYfo="),fit: BoxFit.cover)
+              ),
+            ),
+            const Text("Chats",style:TextStyle(
+              fontSize: 22,
+              color: Colors.black,
+              fontWeight: FontWeight.bold
+            ),),
+            Icon(Icons.edit)
+          ],),
+          const SizedBox(height: 10,),
+          Container(
+            width: double.infinity,
+            height: 40,
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.circular(10)
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 10),
+            child: TextField(
+              cursorColor: Colors.black,
+              controller: _searchController,
+              decoration: const InputDecoration(
+                hintText: "Search",
+                border: InputBorder.none,
+                prefixIcon: Icon(Icons.search,
+                color: Colors.black)
+              ),
+            ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
