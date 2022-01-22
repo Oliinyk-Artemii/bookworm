01e5d6875dce040e1dfce55c631db152195dfd9a
Alex <Alex@DESKTOP-A4E41IH>
2022-01-22 12:52:01 +0200

    Create gradient dialog_page.dart

--- lib/pages/dialog_page.dart
+++ lib/pages/dialog_page.dart
@@ -14,83 +14,69 @@ class _DialogPageState extends State<DialogPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-        backgroundColor: Colors.grey.withOpacity(0.2),
-    elevation: 0,
-    leading: FlatButton(
-    onPressed: () {
-    Navigator.pop(context);
-    },
-    child: Icon(
-    Icons.arrow_back_ios,
-    color: Colors.green,
-    )),
-    title: Row(
-    children: <Widget>[
-    Container(
-    width: 40,
-    height: 40,
-    decoration: BoxDecoration(
-    shape: BoxShape.circle,
-    image: DecorationImage(
-    image: NetworkImage(
-    "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"),
-    fit: BoxFit.cover)),
-    ),
-    SizedBox(
-    width: 15,
-    ),
-    Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: <Widget>[
-    Text(
-    "Tyler Nix",
-    style: TextStyle(
-    fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
-    ),
-    SizedBox(
-    height: 3,
-    ),
-    Text(
-    "Active now",
-    style: TextStyle(color: Colors.black.withOpacity(0.4), fontSize: 14),
-    )
-    ],
-    )
-    ],
-    ),
-    actions: <Widget>[
-    Icon(
-    Icons.phone,
-    color: Colors.green,
-    size: 32,
-    ),
-    SizedBox(
-    width: 15,
-    ),
-    Icon(
-    Icons.camera,
-    color: Colors.green,
-    size: 35,
-    ),
-    SizedBox(
-    width: 8,
-    ),
-    Container(
-    width: 13,
-    height: 13,
-    decoration: BoxDecoration(
-    color: Colors.purple,
-    shape: BoxShape.circle,
-    border: Border.all(color: Colors.white38)),
-    ),
-    SizedBox(
-    width: 15,
-    )
-    ,
-    ]
-    ,
-    ),
+      extendBodyBehindAppBar: true,
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                stops: [0.3, 0.7],
+                colors: [Color(0xccE97EA6), Color(0xccB0EADA)])),
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: FlatButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Icon(
+              Icons.close,
+              color: Colors.white,
+              size: 40,
+            )),
+        title: Row(
+          children: <Widget>[
+            Container(
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      image: NetworkImage(
+                          "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"),
+                      fit: BoxFit.cover)),
+            ),
+            SizedBox(
+              width: 15,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Tyler Nix",
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white
+                    ),
+                ),
+                SizedBox(
+                  height: 3,
+                ),
+                Text(
+                  "Active now",
+                  style: TextStyle(
+                      color: Colors.black.withOpacity(0.4), fontSize: 14),
+                )
+              ],
+            )
+          ],
+        ),
+        actions: <Widget>[
+
+        ],
+      ),
     );
   }
 }
